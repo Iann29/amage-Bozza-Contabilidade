260ab94a0db5a0fae7f5a38b1f4a7f4503ad9470
Iann29 <iansingshau@gmail.com>
2025-04-03 20:02:42 -0300

    Resetando o graph

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import NossosServicos from './components/NossosServicos';
-import AnimacaoGrafico from './components/AnimacaoGrafico';
 import QuestionarioPage from './pages/questionario/QuestionarioPage';
 import SmoothScroll from './components/SmoothScroll';
 
@@ -14,9 +13,6 @@ const HomePage = () => (
     
     {/* Seção de serviços com o novo componente */}
     <NossosServicos />
-    
-    {/* Componente de animação do gráfico */}
-    <AnimacaoGrafico />
   </div>
 );
 
