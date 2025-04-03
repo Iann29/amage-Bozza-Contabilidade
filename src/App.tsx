ab568022078a30eb7811d6f46b32ce01c2f88c5b
Iann29 <iansingshau@gmail.com>
2025-04-03 20:16:35 -0300

    lottielab implementation

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import NossosServicos from './components/NossosServicos';
 import QuestionarioPage from './pages/questionario/QuestionarioPage';
 import SmoothScroll from './components/SmoothScroll';
+import LottieScrollAnimation from './components/LottieScrollAnimation';
 
 // Página inicial simplificada que será reconstruída
 const HomePage = () => (
@@ -13,6 +14,14 @@ const HomePage = () => (
     
     {/* Seção de serviços com o novo componente */}
     <NossosServicos />
+    
+    {/* Animação controlada por scroll */}
+    <div className="py-16 bg-white">
+      <div className="container mx-auto px-4">
+        <h2 className="text-3xl font-bold text-center mb-10 text-[#024570]">Nossos Resultados</h2>
+        <LottieScrollAnimation />
+      </div>
+    </div>
   </div>
 );
 
